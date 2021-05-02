9a12db34fd6f72ec71ac13b668efc1ff92b0eede
Martin Mulyo Syahidin <mulyosyahidin95@gmail.com>
2021-05-03 00:06:30 +0700

    Update database

--- database/toko_sayur.sql
+++ database/toko_sayur.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 02 Bulan Mei 2021 pada 23.21
+-- Waktu pembuatan: 03 Bulan Mei 2021 pada 00.06
 -- Versi server: 10.3.25-MariaDB-0ubuntu0.20.04.1
 -- Versi PHP: 7.4.16
 
@@ -41,7 +41,12 @@ CREATE TABLE `ci_sessions` (
 
 INSERT INTO `ci_sessions` (`id`, `ip_address`, `timestamp`, `data`) VALUES
 ('nh2h85rl0sug37e8dtcfgqrc4d7o9c27', '::1', 1619971464, 0x5f5f63695f6c6173745f726567656e65726174657c693a313631393937313436343b),
-('p1r5m6ad0gho44noleiu3vf40sq4qbk7', '::1', 1619971466, 0x5f5f63695f6c6173745f726567656e65726174657c693a313631393937313436343b);
+('p1r5m6ad0gho44noleiu3vf40sq4qbk7', '::1', 1619973639, 0x5f5f63695f6c6173745f726567656e65726174657c693a313631393937333633393b),
+('glb8ltfrvc6r0uoa4ivpihqs526o8v3p', '::1', 1619974000, 0x5f5f63695f6c6173745f726567656e65726174657c693a313631393937343030303b7265646972656374696f6e7c4e3b),
+('60pa6bnm27hl15arm4qfdnsrj216sac1', '::1', 1619974607, 0x5f5f63695f6c6173745f726567656e65726174657c693a313631393937343630373b7265646972656374696f6e7c4e3b5f5f4143544956455f53455353494f4e5f444154417c733a3235363a2262643634333163646339353634393364663062346535643131663436343638333237666630393330373062626637303634616266323434383334656332383433653831313035333438623631306435306131333663333465373161346163346135633531373031626639383465613730623966353436646236326634313432354e577a71446f3946392f72545037444a4b4733506e4a4e713174544e7157714f6c64544649334c6e4454675545444f504976775933714d4e337670777a556758714e645a66715a44576e722f6b414679506b652b69744670384635655448476c512b4777644f776f714b39335a4e3175364334506f363847445a545162766d33223b),
+('6jj4htoa08uds4ipl88oar78uib9mic0', '::1', 1619974947, 0x5f5f63695f6c6173745f726567656e65726174657c693a313631393937343934373b5f5f4143544956455f53455353494f4e5f444154417c733a3235363a22616334633737653032653531663034653162616331313633303932623438313430323964623666316232303566326331616561616462303335643339616631363036393233643762663264373364363138323536613965323266623737343336356631353165313235346333396236386466393062336662646531343337396561412f67454149716b575a304e325435584a4175534952344244776b6638584241723876524e3468434a77505576485462654379423430346c334b732f496b4b436e4d4d357a707558454b7836627943587737757650527a31396e735a595173312b6e526d6c74687033694e5853363531446c2b336e7971344a674d4b704c45223b),
+('mkpfag5t076lvkraup4j1hl4er3sjbn0', '::1', 1619975084, 0x5f5f63695f6c6173745f726567656e65726174657c693a313631393937343934373b5f5f4143544956455f53455353494f4e5f444154417c733a3235363a2231613663363865316538623564316534326531303630633737653562316266643763333736373065393063363730663131346430323337383430386437613938363833336134613431333139636561316662323730313236363938356337666261366561383739333461613935333965663865313638376265326365313662304b757a5653494c62434e73366f61427a776e3273505158574f7a45304e4a5857704a6d47506458786c643239527731394446585a546141563265755143516e6f4f667746666f31594249346b444d6964636f347845726f6a492f6c4e564b775249476d435a716831326e52547a5a74676738686d507554527442595946325062223b7265646972656374696f6e7c4e3b6f726465725f666c6173687c733a32363a2253746174757320626572686173696c2064697065726261727569223b5f5f63695f766172737c613a313a7b733a31313a226f726465725f666c617368223b733a333a226f6c64223b7d),
+('ojam30rdjfof9fubdov3bdb48s0520i6', '127.0.0.1', 1619975089, 0x5f5f63695f6c6173745f726567656e65726174657c693a313631393937343936393b5f5f4143544956455f53455353494f4e5f444154417c733a3235363a223433336436373235323135626562616438303562366632383166656266306664396430356639656464306462613661643032303230353463323630303138323863633561383630333337323433393930346165653766616536306439646264646333616435383765366335333237643966356133323631626139373036643537365754705675797258396a4d4a306a73456e54744e4e48495771796136334c4c59432b4a5a586a58752b4555726b6c506738466c5856346d637a6763656546794134446741455570793746693857784e51336373745247526c366447704a7a72395573703745657139657633536759564a30586349627161464e326b4c397433223b);
 
 -- --------------------------------------------------------
 
@@ -89,8 +94,8 @@ CREATE TABLE `coupons` (
 --
 
 INSERT INTO `coupons` (`id`, `name`, `code`, `credit`, `start_date`, `expired_date`, `is_active`) VALUES
-(1, '#DiRumahAja', 'DIRUMAHAJA19', '5000.00', '2020-03-27', '2020-04-03', 1),
-(2, 'Buah Merah Meriah', 'BUAHMERAH', '5000.00', '2020-03-27', '2020-03-30', 0);
+(4, 'Berbagi Ramadhan', 'RAMADHAN2021', '5000.00', '2021-05-02', '2021-05-09', 1),
+(5, 'WELCOME MAY', 'MAY21', '4000.00', '2021-05-01', '2021-05-08', 1);
 
 -- --------------------------------------------------------
 
@@ -140,18 +145,15 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `user_id`, `coupon_id`, `order_number`, `order_status`, `order_date`, `total_price`, `total_items`, `payment_method`, `delivery_data`, `delivered_date`, `finish_date`) VALUES
-(5, 7, NULL, 'TJL2932027019', '2', '2020-03-29 08:14:50', '271000.00', 2, 1, '{\"customer\":{\"name\":\"Agung Tri Saputra\",\"phone_number\":\"081328907767\",\"address\":\"Kost Indah Jaya Belakang No. 19, Jl. Medan Baru VI, Kandang Limun, Bengkulu\"},\"note\":\"\"}', NULL, NULL),
-(6, 7, NULL, 'JQZ2932017914', '1', '2020-03-29 08:15:13', '350000.00', 1, 2, '{\"customer\":{\"name\":\"Agung Tri Saputra\",\"phone_number\":\"081328907767\",\"address\":\"Kost Indah Jaya Belakang No. 19, Jl. Medan Baru VI, Kandang Limun, Bengkulu\"},\"note\":\"\"}', NULL, NULL),
-(7, 7, NULL, 'TIB3032037078', '2', '2020-03-30 08:49:42', '78000.00', 3, 1, '{\"customer\":{\"name\":\"Agung Tri Saputra\",\"phone_number\":\"081328907767\",\"address\":\"Kost Indah Jaya Belakang No. 19, Jl. Medan Baru VI, Kandang Limun, Bengkulu\"},\"note\":\"\"}', NULL, NULL),
-(8, 7, NULL, 'GPS3032027749', '1', '2020-03-30 09:14:27', '360000.00', 2, 1, '{\"customer\":{\"name\":\"Agung Tri Saputra\",\"phone_number\":\"081328907767\",\"address\":\"Kost Indah Jaya Belakang No. 19, Jl. Medan Baru VI, Kandang Limun, Bengkulu\"},\"note\":\"\"}', NULL, NULL);
+(9, 7, NULL, 'ZCV352137461', '3', '2021-05-03 00:03:44', '62000.00', 3, 2, '{\"customer\":{\"name\":\"Agung Tri Saputra\",\"phone_number\":\"081328907767\",\"address\":\"Kost Indah Jaya Belakang No. 19, Jl. Medan Baru VI, Kandang Limun, Bengkulu\"},\"note\":\"\"}', NULL, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `order_item`
+-- Struktur dari tabel `order_items`
 --
 
-CREATE TABLE `order_item` (
+CREATE TABLE `order_items` (
   `id` bigint(20) NOT NULL,
   `order_id` bigint(20) DEFAULT NULL,
   `product_id` bigint(20) DEFAULT NULL,
@@ -160,20 +162,13 @@ CREATE TABLE `order_item` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `order_item`
+-- Dumping data untuk tabel `order_items`
 --
 
-INSERT INTO `order_item` (`id`, `order_id`, `product_id`, `order_qty`, `order_price`) VALUES
-(3, 4, 2, 1, '31000.00'),
-(4, 4, 8, 13, '20000.00'),
-(5, 5, 2, 1, '31000.00'),
-(6, 5, 13, 12, '20000.00'),
-(7, 6, 9, 10, '35000.00'),
-(8, 7, 10, 1, '12000.00'),
-(9, 7, 9, 1, '35000.00'),
-(10, 7, 2, 1, '31000.00'),
-(11, 8, 9, 1, '35000.00'),
-(12, 8, 1, 5, '65000.00');
+INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `order_qty`, `order_price`) VALUES
+(13, 9, 9, 1, '35000.00'),
+(14, 9, 10, 1, '12000.00'),
+(15, 9, 11, 1, '15000.00');
 
 -- --------------------------------------------------------
 
@@ -284,25 +279,6 @@ INSERT INTO `reviews` (`id`, `user_id`, `order_id`, `title`, `review_text`, `rev
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `roles`
---
-
-CREATE TABLE `roles` (
-  `id` int(10) NOT NULL,
-  `role_name` varchar(191) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `roles`
---
-
-INSERT INTO `roles` (`id`, `role_name`) VALUES
-(1, 'Admin'),
-(2, 'Customer');
-
--- --------------------------------------------------------
-
---
 -- Struktur dari tabel `settings`
 --
 
@@ -344,7 +320,7 @@ CREATE TABLE `users` (
   `username` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `profile_picture` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `role_id` int(10) DEFAULT 0,
+  `role` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT '0' COMMENT '1 = admin, 2 = customer',
   `register_date` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -352,9 +328,9 @@ CREATE TABLE `users` (
 -- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `username`, `password`, `profile_picture`, `role_id`, `register_date`) VALUES
-(1, 'Admin Toko Sayur', 'admin@local.test', NULL, 'admin', '$2y$10$Brm3RNWFKvZ1e0ej1vBz9.QbFMW21q0l/iDSt5aDOoGj9zlLFuxh6', 'agung1.png', 1, NULL),
-(7, 'Customer Toko Sayur', 'customer@local.test', NULL, 'customer', '$2y$10$6C/A5Yy1gt4yhStWDWN1M.isBaznzDc.MZJdIj7UddW3.qIX5vDvK', NULL, 2, '2020-03-29 08:14:30');
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `username`, `password`, `profile_picture`, `role`, `register_date`) VALUES
+(1, 'Admin Toko Sayur', 'admin@local.test', NULL, 'admin', '$2y$10$Brm3RNWFKvZ1e0ej1vBz9.QbFMW21q0l/iDSt5aDOoGj9zlLFuxh6', 'agung1.png', 'admin', NULL),
+(7, 'Customer Toko Sayur', 'customer@local.test', NULL, 'customer', '$2y$10$6C/A5Yy1gt4yhStWDWN1M.isBaznzDc.MZJdIj7UddW3.qIX5vDvK', NULL, 'customer', '2020-03-29 08:14:30');
 
 --
 -- Indexes for dumped tables
@@ -395,9 +371,9 @@ ALTER TABLE `orders`
   ADD KEY `FK_orders_coupons` (`coupon_id`);
 
 --
--- Indeks untuk tabel `order_item`
+-- Indeks untuk tabel `order_items`
 --
-ALTER TABLE `order_item`
+ALTER TABLE `order_items`
   ADD PRIMARY KEY (`id`),
   ADD KEY `order_id` (`order_id`),
   ADD KEY `product_id` (`product_id`);
@@ -431,12 +407,6 @@ ALTER TABLE `reviews`
   ADD KEY `FK_reviews_orders` (`order_id`);
 
 --
--- Indeks untuk tabel `roles`
---
-ALTER TABLE `roles`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indeks untuk tabel `settings`
 --
 ALTER TABLE `settings`
@@ -448,7 +418,7 @@ ALTER TABLE `settings`
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`) USING BTREE,
   ADD UNIQUE KEY `users_email_unique` (`email`) USING BTREE,
-  ADD KEY `role_id` (`role_id`);
+  ADD KEY `role_id` (`role`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
@@ -464,7 +434,7 @@ ALTER TABLE `contacts`
 -- AUTO_INCREMENT untuk tabel `coupons`
 --
 ALTER TABLE `coupons`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `customers`
@@ -476,13 +446,13 @@ ALTER TABLE `customers`
 -- AUTO_INCREMENT untuk tabel `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT untuk tabel `order_item`
+-- AUTO_INCREMENT untuk tabel `order_items`
 --
-ALTER TABLE `order_item`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+ALTER TABLE `order_items`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT untuk tabel `payments`
@@ -507,12 +477,6 @@ ALTER TABLE `product_category`
 --
 ALTER TABLE `reviews`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT untuk tabel `roles`
---
-ALTER TABLE `roles`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `settings`
@@ -548,6 +512,13 @@ ALTER TABLE `customers`
 ALTER TABLE `orders`
   ADD CONSTRAINT `FK_orders_coupons` FOREIGN KEY (`coupon_id`) REFERENCES `coupons` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `FK_orders_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `order_items`
+--
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
